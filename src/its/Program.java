f173a062c16d7a2fecefb7813282e13c8e00319b
rummolprod999 <rummolprod999@gmail.com>
2020-10-05 18:56:55 +0300

    update project

--- src/its/Program.java
+++ src/its/Program.java
@@ -4,7 +4,20 @@ import static java.lang.System.out;
 
 public class Program {
     public static void main(String... args) {
+        methodReference();
+        lambda();
+    }
+
+    private static void methodReference() {
         var duck = (Quacks & Waddles) Mixin::create;
+        out.println(duck.getClass());
+        duck.quack();
+        duck.waddle();
+    }
+
+    private static void lambda() {
+        var duck = (Quacks & Waddles) (() -> {
+        });
         out.println(duck.getClass());
         duck.quack();
         duck.waddle();
