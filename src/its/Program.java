442cdef3e3d903277801370fbd96bff72f266bad
rummolprod999 <rummolprod999@gmail.com>
2020-10-05 18:50:43 +0300

    update project

--- src/its/Program.java
+++ src/its/Program.java
@@ -1,8 +1,11 @@
 package its;
 
+import static java.lang.System.out;
+
 public class Program {
     public static void main(String... args) {
         var duck = (Quacks & Waddles) Mixin::create;
+        out.println(duck.getClass());
         duck.quack();
         duck.waddle();
     }
